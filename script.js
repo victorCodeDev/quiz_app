c1f3c1b08908c923966abd243a3ffc738ea73099
victor.ojeda <victor.ojeda@konecta.com.py>
2021-02-22 19:45:24 -0300

    Se empieza con el proyecto de quiz app. Se comitea primeros estilos de la pantalla de preguntas

---
+++ script.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: 'Lenguaje de Programación Favorito',
+        a: 'Javascript',
+        b: 'Java',
+        c: 'Python',
+        d: 'C++',
+        correct: 'a'
+    },
+    {
+        question: 'Lenguaje de Programación menos Favorito',
+        a: 'Php',
+        b: 'Java',
+        c: 'C#',
+        d: 'Python',
+        correct: 'a'
+    },
+    {
+        question: 'Primer lenguaje de programación',
+        a: 'C++',
+        b: 'Java',
+        c: 'C',
+        d: 'Python',
+        correct: 'c'
+    },
+    {
+        question: 'Canal de Youtube Favorito',
+        a: 'Julio Profe',
+        b: 'Fazt Code',
+        c: 'Pildoras Informáticas',
+        d: 'Hola Mundo',
+        correct: 'b'
+    },
+    {
+        question: 'Framework Frontend mas usado',
+        a: 'Vue',
+        b: 'React',
+        c: 'Svelte',
+        d: 'Angular',
+        correct: 'd'
+    },
+];
