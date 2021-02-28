ae950f0834a441874275c753c0f37448b8cc9e27
victor.ojeda <victor.ojeda@konecta.com.py>
2021-02-28 17:10:44 -0300

    Se termino de implementar el test de preguntas, indicando el puntaje de aciertos

--- script.js
+++ script.js
@@ -1,42 +1,100 @@
-const questions = [
+const quizData = [
     {
-        question: 'Lenguaje de Programación Favorito',
-        a: 'Javascript',
-        b: 'Java',
-        c: 'Python',
-        d: 'C++',
-        correct: 'a'
+        question: "¿Cuál es el lenguaje de programación más utilizado en 2019?",
+        a: "Java",
+        b: "Go",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
     },
     {
-        question: 'Lenguaje de Programación menos Favorito',
-        a: 'Php',
-        b: 'Java',
-        c: 'C#',
-        d: 'Python',
-        correct: 'a'
+        question: "¿Quién creó el lenguaje Java?",
+        a: "Dennis Ritchie",
+        b: "James Gosling",
+        c: "Bill Gates",
+        d: "Brendan Eich",
+        correct: "b",
     },
     {
-        question: 'Primer lenguaje de programación',
-        a: 'C++',
-        b: 'Java',
-        c: 'C',
-        d: 'Python',
-        correct: 'c'
+        question: "¿Qué significa HTML?",
+        a: "Lenguaje de marcado de hipertexto",
+        b: "Hoja de estilo en cascada",
+        c: "Notación de objetos de Jason",
+        d: "Helicópteros Terminales Lanchas Lamborginis",
+        correct: "a",
     },
     {
-        question: 'Canal de Youtube Favorito',
-        a: 'Julio Profe',
-        b: 'Fazt Code',
-        c: 'Pildoras Informáticas',
-        d: 'Hola Mundo',
-        correct: 'b'
-    },
-    {
-        question: 'Framework Frontend mas usado',
-        a: 'Vue',
-        b: 'React',
-        c: 'Svelte',
-        d: 'Angular',
-        correct: 'd'
+        question: "¿En qué año se lanzó JavaScript?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "Ninguna de las anteriores",
+        correct: "b",
     },
 ];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>Has respondido correctamente ${score}/${quizData.length} preguntas.</h2>
+                
+                <button onclick="location.reload()">Reiniciar</button>
+            `;
+        }
+    }
+});
